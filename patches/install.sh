b2df54a41007a2b6334367792e759fea03a0857a
Joe <joe2k01dev@gmail.com>
2016-12-30 11:43:25 +0100

    patches dir

---
+++ patches/install.sh
@@ -0,0 +1,18 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="frameworks/av"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	#git am $rootdirectory/device/umi/SUPER/patches/$dir/*.patch
+	git apply $rootdirectory/device/umi/SUPER/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory

2ff1ea709ca388a0b3758e9dbdde0f0b6e0e5bfa
Joe <joe2k01dev@gmail.com>
2017-01-24 20:23:21 +0100

    board/ for TWRP

--- board/device.mk
+++
@@ -1,8 +0,0 @@
-# OTA assert
-TARGET_OTA_ASSERT_DEVICE := SUPER,SUPER,UMI,UMI
-
-# Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
-
-#Use dlmalloc instead of jemalloc for mallocs
-MALLOC_IMPL := dlmalloc
--- board/telephony.mk
+++
@@ -1,3 +0,0 @@
-# RIL
-BOARD_RIL_CLASS := ../../../device/umi/SUPER/ril
-BOARD_CONNECTIVITY_MODULE := conn_soc

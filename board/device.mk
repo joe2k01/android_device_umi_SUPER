4ff56ac2151678638b9f9fe27485ed81a9929f0d
Joe <joe2k01dev@gmail.com>
2017-01-27 16:33:37 +0100

    Switch to MALLOC_SVELTE

--- board/device.mk
+++ board/device.mk
@@ -4,5 +4,4 @@ TARGET_OTA_ASSERT_DEVICE := SUPER,SUPER,UMI,UMI
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
-#Use dlmalloc instead of jemalloc for mallocs
-MALLOC_IMPL := dlmalloc
+MALLOC_SVELTE := true

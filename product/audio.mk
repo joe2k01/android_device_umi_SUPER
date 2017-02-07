953aaf02b3bddf83588291ba65c2cbb16e7d297a
leskal <alexakis1997@gmail.com>
2017-02-07 12:35:21 +0200

    Audio makefile

--- product/audio.mk
+++ product/audio.mk
@@ -1,1 +1,11 @@
 # Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio_policy.default \
+    libaudio-resampler \
+    libaudiopolicymanagerdefault \
+    libtinycompress \
+    libtinyxml \
+    tinymix

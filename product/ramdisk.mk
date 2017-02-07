c888d2f29f5529d4378ceaf5649aa0f6569dc334
leskal <alexakis1997@gmail.com>
2017-02-07 19:08:42 +0200

    Remove unused imports

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,22 +1,21 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
-	enableswap.sh \
-	factory_init.project.rc \
-	factory_init.rc \
-	fstab.mt6755 \
-	init.aee.rc \
-	init.modem.rc \
-	init.mt6755.rc \
-	init.mt6755.usb.rc \
-	init.nvdata.rc \
-	init.project.rc \
-	init.recovery.mt6755.rc \
-	init.xlog.rc \
-	meta_init.modem.rc \
-	meta_init.project.rc \
-	meta_init.rc \
-	ueventd.mt6755.rc
-
+    enableswap.sh \
+    factory_init.project.rc \
+    factory_init.rc \
+    fstab.mt6755 \
+    init.modem.rc \
+    init.mt6755.rc \
+    init.mt6755.usb.rc \
+    init.nvdata.rc \
+    init.project.rc \
+    init.recovery.mt6755.rc \
+    init.xlog.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    ueventd.mt6755.rc
+    
 # Properly input of int.rc
 PRODUCT_COPY_FILES := \
 	$(LOCAL_PATH)/rootdir/etc/init.rc:root/init.rc

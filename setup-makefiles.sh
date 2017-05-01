2406afe2329403c12d9c7c35b9c0c4b0abd91863
Joe <joe2k01dev@gmail.com>
2017-05-01 14:21:11 +0200

    setup-makefiles.sh: add device and vendor strings

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+DEVICE=SUPER
+VENDOR=umi
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 

8e72aecf9a769d6e987e330eaadec83af9f8a4f1
Joe <joe2k01dev@gmail.com>
2017-05-01 17:30:17 +0200

    TEST: ramdisk changes

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -37,10 +37,10 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rc
+LOCAL_MODULE       := init.mtk.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rc
+LOCAL_SRC_FILES    := etc/init.mtk.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
